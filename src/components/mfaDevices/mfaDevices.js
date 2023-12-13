93a91849ece5561ccd1dedb7c6259250b3e19552
Warren Parad <gpg@warrenparad.net>
2023-12-13 20:02:13 +0100

    Fix scrollbar.

--- src/components/mfaDevices/mfaDevices.js
+++ src/components/mfaDevices/mfaDevices.js
@@ -177,20 +177,23 @@ export default class MfaDevices extends LitElement {
         border-color: var(--brand-color--bg);
       }
 
+      .device {
+        margin: 0.75rem 0.5rem;
+      }
       .device-list-wrapper {
         max-height: 225px;
       }
-      .device-list::-webkit-scrollbar {
+      .device-list-wrapper::-webkit-scrollbar {
         width: 6px;
         background-color: white;
       }
-      .device-list::-webkit-scrollbar-thumb {
+      .device-list-wrapper::-webkit-scrollbar-thumb {
         background: var(--brand-color--bg-shadow);
         border-radius: 12px;
         border: 2px solid var(--brand-color--bg-shadow);
       }
 
-      .device-list::-webkit-scrollbar-track {
+      .device-list-wrapper::-webkit-scrollbar-track {
         background: white;
       }
 
@@ -337,7 +340,7 @@ export default class MfaDevices extends LitElement {
 
   deviceDisplay(device, displayTrash = false) {
     return html`
-      <div class="d-flex align-items-center justify-content-between p-3 m-3" style="border-color: var(--info); box-shadow: 2px 2px 10px 5px var(--brand-color--bg-shadow);">
+      <div class="device d-flex align-items-center justify-content-between p-3" style="border-color: var(--info); box-shadow: 2px 2px 10px 5px var(--brand-color--bg-shadow);">
         
         <div class="d-flex">
           <div class="d-flex align-items-center me-2">
@@ -377,12 +380,27 @@ export default class MfaDevices extends LitElement {
 
       <div class="d-flex justify-content-center">
         <div>
+
+          ${!this.devices.length && html`
+            <div class="d-flex flex-grow-1 justify-content-around mt-3">
+              <div class="d-flex align-items-center">
+                <button class="back-button btn btn-sm" @click="${() => { this.state = states.NEW; this.requestUpdate(); }}">
+                  <div class="d-flex flex-column justify-content-around p-3">
+                    <div class="mb-2">
+                      <svg style="height:32px; width=32px;" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" fill="#000000"><g> <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage"> <g sketch:type="MSLayerGroup" transform="translate(-464.000000, -1087.000000)" fill="#000000"> <path d="M480,1117 C472.268,1117 466,1110.73 466,1103 C466,1095.27 472.268,1089 480,1089 C487.732,1089 494,1095.27 494,1103 C494,1110.73 487.732,1117 480,1117 L480,1117 Z M480,1087 C471.163,1087 464,1094.16 464,1103 C464,1111.84 471.163,1119 480,1119 C488.837,1119 496,1111.84 496,1103 C496,1094.16 488.837,1087 480,1087 L480,1087 Z M486,1102 L481,1102 L481,1097 C481,1096.45 480.553,1096 480,1096 C479.447,1096 479,1096.45 479,1097 L479,1102 L474,1102 C473.447,1102 473,1102.45 473,1103 C473,1103.55 473.447,1104 474,1104 L479,1104 L479,1109 C479,1109.55 479.447,1110 480,1110 C480.553,1110 481,1109.55 481,1109 L481,1104 L486,1104 C486.553,1104 487,1103.55 487,1103 C487,1102.45 486.553,1102 486,1102 L486,1102 Z" sketch:type="MSShapeGroup"> </path> </g> </g> </g></svg>
+                    </div>
+                    <div>Add device</div>
+                  </div>
+                </button>
+              </div>
+            </div>`
+          || html`
           <button class="create-button btn btn-sm" @click="${() => { this.state = states.NEW; this.requestUpdate(); }}">
             <span class="d-flex align-items-baseline">
               <svg style="margin-right: 0.25rem; margin-top: 0.25rem; height:12px; width=12px;" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" fill="#000000"><g> <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage"> <g sketch:type="MSLayerGroup" transform="translate(-464.000000, -1087.000000)" fill="#000000"> <path d="M480,1117 C472.268,1117 466,1110.73 466,1103 C466,1095.27 472.268,1089 480,1089 C487.732,1089 494,1095.27 494,1103 C494,1110.73 487.732,1117 480,1117 L480,1117 Z M480,1087 C471.163,1087 464,1094.16 464,1103 C464,1111.84 471.163,1119 480,1119 C488.837,1119 496,1111.84 496,1103 C496,1094.16 488.837,1087 480,1087 L480,1087 Z M486,1102 L481,1102 L481,1097 C481,1096.45 480.553,1096 480,1096 C479.447,1096 479,1096.45 479,1097 L479,1102 L474,1102 C473.447,1102 473,1102.45 473,1103 C473,1103.55 473.447,1104 474,1104 L479,1104 L479,1109 C479,1109.55 479.447,1110 480,1110 C480.553,1110 481,1109.55 481,1109 L481,1104 L486,1104 C486.553,1104 487,1103.55 487,1103 C487,1102.45 486.553,1102 486,1102 L486,1102 Z" sketch:type="MSShapeGroup"> </path> </g> </g> </g></svg>
               Add device
             </span>
-          </button>
+          </button>`}
         </div>
       </div>
     
